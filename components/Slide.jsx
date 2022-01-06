7496219ad7b85894f882c3ed6e8e771758a557de
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-07 01:57:02 +0900

    feat: Swiper를 이용한 Slide Component

---
+++ components/Slide.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { StyleSheet, useColorScheme, View } from "react-native";
+import styled from "styled-components/native";
+import { makeImgPath } from "../utils";
+import { BlurView } from "@react-native-community/blur";
+import Poster from "./Poster";
+
+const BgImg = styled.Image``;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Column = styled.View`
+  width: 45%;
+  margin-left: 15px;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  font-weight: 600;
+  color: ${(props) => (props.isDark ? "white" : "black")};
+`;
+
+const OverView = styled.Text`
+  margin-top: 10px;
+  color: ${(props) => (props.isDark ? "#dfdbdb" : "black")};
+`;
+
+const Votes = styled(OverView)`
+  font-size: 12px;
+`;
+
+const Slide = ({
+  backdropPath,
+  posterPath,
+  movieTitle,
+  voteAverage,
+  overview,
+}) => {
+  const isDark = useColorScheme() === "dark";
+  return (
+    <View style={{ flex: 1 }}>
+      <BgImg
+        style={StyleSheet.absoluteFill}
+        source={{ uri: makeImgPath(backdropPath) }}
+      />
+      <BlurView
+        style={StyleSheet.absoluteFill}
+        blurType={isDark ? "dark" : "light"}
+        blurAmount={10}
+      >
+        <Wrapper>
+          <Poster path={posterPath} />
+          <Column>
+            <Title isDark={isDark}>{movieTitle}</Title>
+            {voteAverage > 0 && (
+              <Votes isDark={isDark}>⭐️ {voteAverage} / 10</Votes>
+            )}
+            <OverView isDark={isDark}>{overview.slice(0, 90)}...</OverView>
+          </Column>
+        </Wrapper>
+      </BlurView>
+    </View>
+  );
+};
+
+export default Slide;
