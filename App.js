116bd460343c191aa4028f17dcd16c6603787d99
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-06 23:22:20 +0900

    feat: Use ThemeProvider and Use useColorTheme

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
+} from "@react-navigation/native";
 import Root from "./navigation/Root";
+import { ThemeProvider } from "styled-components";
+import { useColorScheme } from "react-native";
+import { darkTheme, ligthTheme } from "./theme";
 
 export default function App() {
+  const isDark = useColorScheme() === "dark" && true;
+
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : ligthTheme}>
+      <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
