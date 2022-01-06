56733546ed3c374c8bff4c992235a34efbaba0c1
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-06 20:24:39 +0900

    feat: One Component

---
+++ screens/One.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const One = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>One</Text>
+    </View>
+  );
+};
+
+export default One;
