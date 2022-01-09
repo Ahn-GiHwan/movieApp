bf113e576669887f9a6c33f08ecca93248c2cc13
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-09 23:22:56 +0900

    feat: import react-query
    Add code
    - import { QueryClient, QueryClientProvider } from "react-query";
    - const queryClient = new QueryClient();
    - <QueryClientProvider client={queryClient}> ... </QueryClientProvider>

--- App.js
+++ App.js
@@ -8,15 +8,20 @@ import Root from "./navigation/Root";
 import { ThemeProvider } from "styled-components";
 import { useColorScheme } from "react-native";
 import { darkTheme, ligthTheme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   const isDark = useColorScheme() === "dark" && true;
 
   return (
-    <ThemeProvider theme={isDark ? darkTheme : ligthTheme}>
-      <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
-        <Root />
-      </NavigationContainer>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={isDark ? darkTheme : ligthTheme}>
+        <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
+          <Root />
+        </NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
