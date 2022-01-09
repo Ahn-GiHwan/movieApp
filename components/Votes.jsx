7bfa7144e024e5fd6a059205d8e1529e53c8f044
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-09 21:42:49 +0900

    refact: Vote Component

---
+++ components/Votes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const VoteText = styled.Text`
+  margin-top: 5px;
+  color: ${(props) => props.theme.textColor};
+  font-size: 10px;
+`;
+
+const Votes = ({ voteAverage }) => (
+  <VoteText>
+    {voteAverage > 0 ? `⭐️ ${voteAverage} / 10` : "Coming soon..."}
+  </VoteText>
+);
+
+export default Votes;
