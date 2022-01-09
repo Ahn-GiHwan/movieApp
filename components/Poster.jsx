eaa1c7162f081588b341f7c276cdb30859c0011f
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-09 21:42:35 +0900

    refact: Poster Component

--- components/Poster.jsx
+++ components/Poster.jsx
@@ -9,8 +9,29 @@ const Image = styled.Image`
   background-color: gray;
 `;
 
-const Poster = ({ path }) => {
-  return <Image source={{ uri: makeImgPath(path) }} />;
-};
+const AltImage = styled.View`
+  width: 100px;
+  height: 160px;
+  border-radius: 5px;
+  background-color: gray;
+  justify-content: center;
+  align-items: center;
+`;
 
+const AltText = styled.Text`
+  color: white;
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+const Poster = ({ path }) => {
+  if (path) return <Image source={{ uri: makeImgPath(path) }} />;
+  else {
+    return (
+      <AltImage>
+        <AltText>No Image</AltText>
+      </AltImage>
+    );
+  }
+};
 export default Poster;
