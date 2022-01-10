ce130e2107217205ef0da9f268808acfb662563d
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-10 11:58:24 +0900

    feat: Tv Component
    use
    - react-query
    - HList Component
    - Loader Component

--- screens/Tv.tsx
+++ screens/Tv.tsx
@@ -1,21 +1,50 @@
 import React from "react";
-import styled from "styled-components/native";
-
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
-`;
+import { RefreshControl, ScrollView } from "react-native";
+import { QueryClient, useQuery } from "react-query";
+import { tvApi } from "../api";
+import HList from "../components/HList";
+import Loader from "../components/Loader";
 
 const Tv = () => {
+  const queryClient = new QueryClient();
+
+  const {
+    isLoading: todayIsLoading,
+    data: todayData,
+    isFetching: todayIsFetching,
+  } = useQuery(["tv", "today"], tvApi.airingToday);
+
+  const {
+    isLoading: topIsLoading,
+    data: topData,
+    isFetching: topIsFetching,
+  } = useQuery(["tv", "top"], tvApi.topRated);
+
+  const {
+    isLoading: trendingIsLoading,
+    data: trendingData,
+    isFetching: trendingIsFetching,
+  } = useQuery(["tv", "trending"], tvApi.trending);
+
+  const loading = todayIsLoading || topIsLoading || trendingIsLoading;
+  const refreshing = todayIsFetching || topIsFetching || trendingIsFetching;
+
+  const onRefresh = () => {
+    queryClient.refetchQueries(["tv"]);
+  };
+
+  if (loading) return <Loader />;
   return (
-    <Container>
-      <Title>TV</Title>
-    </Container>
+    <ScrollView
+      contentContainerStyle={{ paddingVertical: 30 }}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+    >
+      <HList title="Trending TV" data={trendingData.results} />
+      <HList title="Airing Today" data={todayData.results} />
+      <HList title="Top Rated TV" data={topData.results} />
+    </ScrollView>
   );
 };
 
