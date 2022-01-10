e617733bffcf601f6f2e53c4c86495114cc89a8c
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-10 13:19:19 +0900

    feat: update search logic
    { enabled: false }를 통해 자동검색을 방지

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
+import { useQuery } from "react-query";
 import styled from "styled-components/native";
+import { searchApi } from "../api";
+import HList from "../components/HList";
+import Loader from "../components/Loader";
 
-const Container = styled.ScrollView`
-  /* flex: 1;
-  justify-content: center;
-  align-items: center; */
-`;
+const Container = styled.ScrollView``;
 
 const SearchBar = styled.TextInput.attrs({
   placeholder: "Search for Movie or TV",
@@ -17,18 +17,45 @@ const SearchBar = styled.TextInput.attrs({
   border-radius: 15px;
   width: 90%;
   margin: 10px auto;
+  margin-bottom: 40px;
 `;
 
 const Search = () => {
   const [query, setQuery] = useState("");
 
+  const {
+    isLoading: moviesLoding,
+    data: moviesData,
+    refetch: moviesSearch,
+  } = useQuery(["searchMovies", query], searchApi.movies, { enabled: false });
+  const {
+    isLoading: tvsLoading,
+    data: tvsData,
+    refetch: tvsSearch,
+  } = useQuery(["searchTvs", query], searchApi.tvs, {
+    enabled: false,
+  });
+
   const onChangeQuery = (text: string) => setQuery(text);
 
-  console.log(query);
+  const onSubmit = () => {
+    if (!query) return;
+    moviesSearch();
+    tvsSearch();
+  };
+
+  const isLoading = moviesLoding || tvsLoading;
 
   return (
     <Container>
-      <SearchBar onChangeText={onChangeQuery} value={query} />
+      <SearchBar
+        onChangeText={onChangeQuery}
+        value={query}
+        onSubmitEditing={onSubmit}
+      />
+      {isLoading && <Loader />}
+      {moviesData && <HList title="Movie Results" data={moviesData.results} />}
+      {tvsData && <HList title="TV Results" data={tvsData.results} />}
     </Container>
   );
 };
