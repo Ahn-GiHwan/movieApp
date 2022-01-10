b8b5e8d93e82c066114e78f8c2c81bdee5939e4e
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-10 14:52:49 +0900

    feat: Add TouchableOpacity and params fullData push `

--- components/VMedia.tsx
+++ components/VMedia.tsx
@@ -1,15 +1,18 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
+import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 import Poster from "./Poster";
 import Votes from "./Votes";
 
 interface VMediaPorps {
-  posterPath: string,
-  title: string,
-  voteAverage: number
+  posterPath: string;
+  title: string;
+  voteAverage: number;
+  fullData: any;
 }
 
-const Movie = styled.View`
+const Container = styled.View`
   align-items: center;
 `;
 
@@ -19,16 +22,32 @@ const Title = styled.Text`
   margin-top: 7px;
 `;
 
-const VMedia: React.FC<VMediaPorps> = ({ posterPath, title, voteAverage }) => {
+const VMedia: React.FC<VMediaPorps> = ({
+  posterPath,
+  title,
+  voteAverage,
+  fullData,
+}) => {
+  const navigation = useNavigation();
+  const goToDeatil = () => {
+    navigation.navigate("Stack", {
+      screen: "Detail",
+      params: {
+        ...fullData,
+      },
+    });
+  };
   return (
-    <Movie>
-      <Poster path={posterPath} />
-      <Title>
-        {title.slice(0, 8)}
-        {title.length > 8 && "..."}
-      </Title>
-      <Votes voteAverage={voteAverage} />
-    </Movie>
+    <TouchableOpacity onPress={goToDeatil}>
+      <Container>
+        <Poster path={posterPath} />
+        <Title>
+          {title.slice(0, 8)}
+          {title.length > 8 && "..."}
+        </Title>
+        <Votes voteAverage={voteAverage} />
+      </Container>
+    </TouchableOpacity>
   );
 };
 
